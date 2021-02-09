7be342a0b20ee02d69f9d420a5891c1648834a6a
marsel <marselinus95@gmail.com>
2021-02-09 10:56:10 +0700

    Membuat Component dengan Class Component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image, TextInput } from 'react-native';
 
 
@@ -12,6 +12,8 @@ const App = () => {
       <Text>penyox</Text>
       <Poto />
       <TextInput style={{ borderWidth: 1 }} />
+      <BoxGreen />
+      <Profile />
     </View>
   );
 };
@@ -25,6 +27,25 @@ const Poto = () => {
     source={{ uri: 'https://placeimg.com/100/100/tech' }}
     style={{ width: 100, height: 100 }} />
 
+};
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini Component dri class</Text>
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{ uri: 'https://placeimg.com/100/100/animals' }}
+          style={{ width: 100, height: 100 }} />
+        <Text style={{ color: 'blue', fontsize: '24' }}>Gambar dari class</Text>
+      </View>
+    );
+  }
 }
 
 export default App;
